4af7629545139af84e491ef0969afe0f1516ff76
Tracy Speed <tspeed@me.com>
2018-08-16 21:27:44 -0500

    add javascript tools

---
+++ index.js
@@ -0,0 +1,12 @@
+var moment = require('moment')
+
+console.log('Im in javascript')
+
+console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+
+// import * as math from 'mathjs'
+
+// console.log(math.eval('1.2 * (2 + 4.5)'));
+
+var name = "Michelle", time = "today";
+console.log(`Hello ${name}, how are you ${time}?`);
